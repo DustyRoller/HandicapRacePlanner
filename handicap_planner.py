47fee91194cebe68c604ee4cb680f7a1ba1437e9
DustyRoller <DustyRoller@github.com>
2026-06-27 22:02:11 +0100

    initial change

--- handicap_planner.py
+++ handicap_planner.py
@@ -10,6 +10,42 @@ class HandicapEntry:
     start_time: timedelta = timedelta()
 
 
+@dataclass
+class HandicapResult:
+    name: str
+    estimated_time: timedelta
+    position: int
+    split_time: timedelta
+    finish_time: timedelta = timedelta()
+
+
+def calculate_result_data(input_file: Path) -> list[HandicapResult]:
+    handicap_results: list[HandicapResult] = []
+
+    with input_file.open() as f:
+        for line in f.readlines():
+            name, time_str, position_str, split_str = map(str, line.split(" - "))
+            est_minutes, est_seconds = map(int, time_str.split(":"))
+            estimated_time: timedelta = timedelta(minutes=est_minutes, seconds=est_seconds)
+            split_minutes, split_seconds = map(int, split_str.split(":"))
+            split_time: timedelta = timedelta(minutes=split_minutes, seconds=split_seconds)
+            handicap_results.append((HandicapResult(name, estimated_time, int(position_str), split_time)))
+
+    if not handicap_results:
+        raise RuntimeError(f"Failed to read any values from {input_file}")
+
+    sorted_handicap_results = sorted(handicap_results, key=lambda x: x.position)
+
+    # Calculate the actual finish times.
+    for i, result in enumerate(sorted_handicap_results):
+        if i == 0:
+            result.finish_time = result.split_time
+        else:
+            result.finish_time = sorted_handicap_results[i - 1].finish_time + result.split_time
+
+    return sorted_handicap_results
+
+
 def calculate_start_times(input_file: Path) -> list[HandicapEntry]:
     handicap_entries: list[HandicapEntry] = []
 
@@ -39,20 +75,34 @@ def calculate_start_times(input_file: Path) -> list[HandicapEntry]:
 
 if __name__ == "__main__":
     import sys
-    from argparse import ArgumentParser, Namespace
+    from argparse import ArgumentParser, Namespace, _MutuallyExclusiveGroup
 
     parser: ArgumentParser = ArgumentParser(
                     prog='Handicap race planner',
                     description='Plan a handicap race')
 
-    parser.add_argument("--times", required=True, type=Path, help="Path to the file with predicted times")
+    query_group: _MutuallyExclusiveGroup = parser.add_mutually_exclusive_group(required=True)
+    query_group.add_argument("--times", type=Path, help="Path to the file with predicted times")
+    query_group.add_argument("--results", type=Path, help="Path to the file with the result times")
 
     args: Namespace = parser.parse_args()
 
-    if not args.times.is_file():
-        sys.exit(f"File does not exist: {args.times}")
+    if args.times:
+        if not args.times.is_file():
+            sys.exit(f"File does not exist: {args.times}")
 
-    entries: list[HandicapEntry] = calculate_start_times(args.times)
+        entries: list[HandicapEntry] = calculate_start_times(args.times)
 
-    for entry in entries:
-        print(f"{entry.name} - {entry.start_time}")
+        for entry in entries:
+            print(f"{entry.name} - {entry.start_time}")
+    else:
+        if not args.results.is_file():
+            sys.exit(f"File does not exist: {args.results}")
+
+        results: list[HandicapResult] = calculate_result_data(args.results)
+
+        for result in results:
+            diff: timedelta = result.finish_time - result.estimated_time
+            sign: str = "+" if diff.total_seconds() >= 0 else "-"
+
+            print(f"{result.name} - {result.estimated_time} - {result.finish_time} - {sign}{abs(diff)}")
